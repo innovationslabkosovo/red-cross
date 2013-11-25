31b57372b5bcfb90781ccb03508d0f302cc5aab8
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2013-11-25 16:54:38 +0100

    BASE_URL - to include enxternal files and links inside website (js, css)  - to include php files inside a php file

--- core/init.php
+++ core/init.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
-$application_name = "red_cross";
 
-$project_root = $_SERVER[DOCUMENT_ROOT].DIRECTORY_SEPARATOR.$application_name.DIRECTORY_SEPARATOR;
+$host = $_SERVER['HTTP_HOST'];
+$application_name = explode("/",$_SERVER['REQUEST_URI']);
+$application_name = $application_name[1];
+
+define('BASE_URL', 'http://'.$host.DIRECTORY_SEPARATOR.$application_name);
+
+$project_root = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.$application_name.DIRECTORY_SEPARATOR;
 
 include $project_root.'config/config.php';
 include $project_root.'core/general.php';
