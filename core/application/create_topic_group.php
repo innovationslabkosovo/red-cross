4a9a65e4639773601a1faf7618173c40fc21269b
nori <arbnorh@gmail.com>
2013-12-03 11:12:40 +0100

    adding first draft of create topic group

--- core/application/create_topic_group.php
+++ core/application/create_topic_group.php
@@ -3,16 +3,22 @@ include '../../config/config.php';
 if(empty($_POST) === false)
 {
 
-   /* $location=$_POST["location"];
-    $date_from=$_POST["date_from"];
-    $date_to=$_POST["date_to"];
-    $test=$_POST["test"];
-    $trainer=$_POST["trainer"];
-    $class_name = "Kursi".$location;
+    $topic_group=$_POST["topic_group"];
 
-    if (mysql_query("INSERT INTO Class(class_id, name, trainer_id, location_id, test_id, date_from, date_to) VALUES ('', '$class_name' ,  '$trainer', '$location', '$test', '$date_from', '$date_to')"))
-        header("location: ../../views/create_class.php?message=success&object=Klasa");
-    else header("location: ../../views/create_class.php?message=fail&object=Klasa");*/
+    if(isset($_POST['active']) &&
+        $_POST['active'] == 'active')
+    {
+        $active = 1;
+    }
+    else
+    {
+        $active = 0;
+    }
+
+
+    if (mysql_query("INSERT INTO TopicGroup(name, active) VALUES ('', '$topic_group' ,  '$active')"))
+        header("location: ../../views/create_topic_group.php?message=success&object=TopicGroup");
+    else header("location: ../../views/create_topic_group.php?message=fail&object=TopicGroup");
 
 
 }
