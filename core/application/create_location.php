a1279acc15152ccfec7f0dfa7aae9e12b2ec3b28
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2013-12-10 13:53:42 +0100

    second draft of create_location

--- core/application/create_location.php
+++ core/application/create_location.php
@@ -5,12 +5,10 @@ if(empty($_POST) === false)
     $name = $_POST['name'];
     $latitude = $_POST['lat'];
     $longitude = $_POST['lon'];
-    $municipality_id = $_POST['komuna'];
+    $municipality_id = $_POST['municipality'];
 
     if (mysql_query("INSERT INTO Location(location_id, name, latitude, longitude, municipality_id) VALUES ('', '$name', '$latitude', '$longitude', '$municipality_id')"))
         header("location: ../../views/create_location.php?message=success&object=location");
     else
         header("location: ../../views/create_location.php?message=fail&object=location");
-
-
 }
--- views/create_location.php
+++ views/create_location.php
@@ -12,60 +12,46 @@ protect_page();
 $errors = array();
 include 'layout/header.php';
 
-$get_municipalities = "SELECT municipality_id, name FROM Municipality ";
+$get_municipalities = "SELECT municipality_id, name, coords FROM Municipality ";
 $municipalities = mysql_query($get_municipalities);
 ?>
 
-<script type="text/javascript" src="http://openlayers.org/dev/OpenLayers.js"></script>
-<meta charset=utf-8 />
-<title>Red Cross - Lokacionet</title>
-<style>
-  article, aside, figure, footer, header, hgroup, 
-  menu, nav, section { display: block; }
-</style>
+    <script type="text/javascript" src="http://openlayers.org/dev/OpenLayers.js"></script>
+    <meta charset=utf-8/>
+    <title>Red Cross - Lokacionet</title>
+    <style>
+        article, aside, figure, footer, header, hgroup,
+        menu, nav, section {
+            display: block;
+        }
+    </style>
 
-<style id="jsbin-css">
+    <form name="input" action="<?php echo BASE_URL; ?>/core/application/create_location.php" method="post">
+        <h1>Shto nje lokacion te ri</h1>
 
-</style>
+        <div id="map" class="smallmap" style="width:625px; height:350px"></div>
+        <input type="hidden" id="lat" name="lat">
+        <input type="hidden" id="lon" name="lon">
 
-
-<form name="input" action="<?php echo BASE_URL; ?>/core/application/create_location.php" method="post">
-<h1>Shto nje lokacion te ri</h1>
-<table border="0">
-<tr>
-<td>Komuna:</td>
-<td>
-	<select name="komuna">
-        <option value="">--Zgjedh Komunen--</option>
-        <?php
-            create_options($municipalities, "municipality_id", "name");
-        ?>
-	</select>
-</td>
-</tr>
-<tr>
-<td>Lokacioni: </td><td><input type="text" name="name"></td></tr>
-
-<tr colspan="2">
-<div id="map" class="smallmap" style="width:625px; height:350px"></div>
-</tr>
-<tr><td><input type="hidden" id="lat" name="lat"></td></tr>
-<tr><td><input type="hidden" id="lon" name="lon"></td></tr>
-<tr><td>&nbsp;</td><td><input type ="submit" value="Ruaj!"></td></tr>
-
-</table>
-</form>
-
+        <br/>
+        <label for="select_city"></label>
+        <select name="municipality" id="select_city">
+            <option value="">-Zgjidhni Qytetin-</option>
+            <?php
+            create_options_municipality($municipalities, "municipality_id", "name", "coords");
+            ?>
+        </select><br/><br/>
+        <label for="name"></label>
+        <input type="text" name="name" id="name" value="-Lokacioni-" onfocus="if(this.value == '-Lokacioni-') this.value=''"><br/><br/>
+        <input type="submit" value="Ruaj!">
+    </form>
 <?php
-$query = "SELECT municipality_id FROM Location WHERE municipality_id = :muni";
+if (isset($_GET['message']) && isset($_GET['object'])) {
+    echo $display_messages[$_GET['object']][$_GET['message']];
+}
 ?>
 
-<?php
-    if (isset($_GET['message']) && isset($_GET['object'])) {
-        echo $display_messages[$_GET['object']][$_GET['message']];
-    }
-?>
-
-<script type="text/javascript" src="<?php echo BASE_URL; ?>/js/map.js"></script>
+    <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/map.js"></script>
+    <script type="text/javascript"> $.validate(); </script>
 
 <?php include $project_root . 'views/layout/footer.php'; ?>
