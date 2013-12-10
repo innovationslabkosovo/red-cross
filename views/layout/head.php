41fc071bd1317047194a0ecede29c9fbcd90f89b
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2013-12-10 13:51:57 +0100

    Add Jquery Validator

--- views/layout/head.php
+++ views/layout/head.php
@@ -5,5 +5,6 @@
     <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/jquery-1.10.2.js"></script>
     <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/jquery.ui.datepicker.js"></script>
     <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/main.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.1.27/jquery.form-validator.min.js"></script>
 </head>
 
