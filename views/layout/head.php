40cd09b917a17dbe75dd74b9bddc4b000c90123a
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2013-12-23 13:03:13 +0100

    update jquery

--- views/layout/head.php
+++ views/layout/head.php
@@ -2,7 +2,6 @@
     <title><?php echo $page_title; ?></title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/screen.css">
-<!--    <script type="text/javascript" src="--><?php //echo BASE_URL; ?><!--/js/jquery-1.10.2.js"></script>-->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/jquery.ui.datepicker.js"></script>
     <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/main.js"></script>
