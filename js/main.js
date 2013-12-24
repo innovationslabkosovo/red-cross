666d81c09535db358ee63d58f601774b822982f5
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2013-12-24 14:48:16 +0100

    update main.js with edit function

--- js/main.js
+++ js/main.js
@@ -5,3 +5,38 @@
 $(function() {
     $("#datefrom, #dateto").datepicker();
 });
+
+$(".edit_tr").on("click", function () {
+    var ID = $(this).attr('id');
+    $("td").find("#results_" + ID).hide();
+    $("td").find("#editbox_" + ID).attr("style","display:block");
+    $("td").find("#" + ID).attr("style","display:block");
+});
+
+$(".save").click(function () {
+        ID = $(this).attr("id");
+        var fileUrl = $("#url").attr("url");
+        var first = $("td").find("#editbox_" + ID);
+        var dataString = $("td").find("#editbox_" + ID).serialize();
+            $.ajax({
+                type: "POST",
+                url: fileUrl,
+                data: dataString,
+                cache: false,
+                dataType: "html",
+                async: false,
+                success: function (html) {
+                    alert("U editua me sukses!");
+                    location.reload();
+                }
+            });
+    });
+
+$(".editbox").mouseup(function () {
+    return false;
+});
+
+$(document).mouseup(function (){
+    $(".editbox, .save").hide();
+    $(".text").show();
+});
