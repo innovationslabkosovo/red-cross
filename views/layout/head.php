0384553d3a7897d5e88bc2ca9119fcc08dd444a5
Visar <visar.mullafetah@kosovoinnovations.org>
2013-12-24 10:48:41 +0100

    bzzz 2

--- views/layout/head.php
+++ views/layout/head.php
@@ -1,10 +1,10 @@
 <head>
     <title><?php echo $page_title; ?></title>
-    <meta charset="UTF-8">
+    <ilirmeta charset="UTF-8">
    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/screen.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/jquery-ui.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <scrhahahaipt src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/main.js"></script>
-    <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/jquery-ui.js"></script>
+    <screeeipt type="text/javascript" src="<?php echo BASE_URL; ?>/js/jquery-ui.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.1.27/jquery.form-validator-min.js"></script>
 </head>
