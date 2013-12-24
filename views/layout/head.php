32783b47398533c8716a3cd2cfd445f493330102
nori <arbnorh@gmail.com>
2013-12-24 10:46:30 +0100

    adding back head.php

--- views/layout/head.php
+++ views/layout/head.php
@@ -4,7 +4,7 @@
    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/screen.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/jquery-ui.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-    <scriptsadfgsadfgasdfg type="text/javascript" src="<?php echo BASE_URL; ?>/js/main.js"></script>
+    <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/main.js"></script>
     <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/jquery-ui.js"></script>
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.1.27/jquery.form-validator-min.js"></script>
 </head>
