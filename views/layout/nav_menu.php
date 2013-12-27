08d22483aa77d829eef34b1973ed817f2d08d39e
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2013-12-27 15:05:10 +0100

    include latest changes on nav_menu

--- views/layout/nav_menu.php
+++ views/layout/nav_menu.php
@@ -24,10 +24,38 @@
     <!-- Top Menu-->
     <li>
         Manage and Create Topics
-        <ul>
+        <ul id="sub_menu">
             <!-- Child Menu -->
             <li><a href="<?php echo BASE_URL; ?>/views/create_topic_group.php">Manage Topic Groups</a></li>
             <li><a href="<?php echo BASE_URL; ?>/views/create_topic.php">Manage Topics</a></li>
         </ul>
     </li>
 </ul>
+
+
+<style>
+    .active {
+        color: #767676 !important;
+    }
+</style>
+
+<?php
+$pages = array();
+$pages["index.php"] = "Home";
+$pages["create_class.php"] = "Class";
+$pages["frauensauna.php"] = "Frauensauna";
+$pages["custom.php"] = "Beauty Lounge";
+$pages["feiertage.php"] = "Feiertage";
+
+$current_page = explode("/", $_SERVER['SCRIPT_NAME']);
+$activePage = $current_page[3];
+$views = 'views';
+?>
+
+<?php foreach($pages as $url=>$title):?>
+    <li>
+        <a <?php if($url === $activePage):?>class="active"<?php endif;?> href="<?php echo BASE_URL.DIRECTORY_SEPARATOR.$views.DIRECTORY_SEPARATOR.$url;?>">
+            <?php echo $title;?>
+        </a>
+    </li>
+<?php endforeach;?>
