99376b0dc73da9569fcbd056284f038431fb6a4e
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2013-12-27 13:43:34 +0100

    Include Nav Menu with children Include css needed for Nav Menu

--- views/layout/nav_menu.php
+++ views/layout/nav_menu.php
@@ -15,3 +15,18 @@
         <li><a href="<?php echo BASE_URL; ?>/views/create_topic.php">Manage Topics</a></li>
     </ul>
 </nav>
+<ul id="menu">
+    <!-- Top Menu without children -->
+    <li><a href="<?php echo BASE_URL; ?>/views/index.php">Home</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/views/create_class.php">Class</a></li>
+    <li><a href="<?php echo BASE_URL; ?>/views/create_location.php">Locations</a></li>
+    <!-- Top Menu-->
+    <li>
+        Manage and Create Topics
+        <ul>
+            <!-- Child Menu -->
+            <li><a href="<?php echo BASE_URL; ?>/views/create_topic_group.php">Manage Topic Groups</a></li>
+            <li><a href="<?php echo BASE_URL; ?>/views/create_topic.php">Manage Topics</a></li>
+        </ul>
+    </li>
+</ul>
