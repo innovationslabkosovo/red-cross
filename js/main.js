62dc02cc18da0d92bd56a18aa466248289b68e7e
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2013-12-30 15:15:24 +0100

    update main js with edit button

--- js/main.js
+++ js/main.js
@@ -2,47 +2,48 @@
  * Created by visar on 11/21/13.
  */
 
-$(function() {
+$(function () {
     $("#datefrom, #dateto, .date_topic").datepicker({dateFormat: 'yy-mm-dd' });
     $(".time_topic").timepicker();
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-$(".edit_tr").not("a").on("click", function () {
-    var ID = $(this).attr('id');
-    var td = $("td");
-    td.find("#results_" + ID).hide();
-    td.find("#editbox_" + ID).attr("style","display:block");
-    td.find("#" + ID).attr("style","display:inline");
-});
-
-$(".save").click(function () {
-        ID = $(this).attr("id");
-        td = $("td");
-        var fileUrl = $("#url").attr("url");
-        //var first = td.find("#editbox_" + ID);
-        var dataString = td.find("#editbox_" + ID).serialize();
-            $.ajax({
-                type: "POST",
-                url: fileUrl,
-                data: dataString,
-                cache: false,
-                dataType: "html",
-                async: false,
-                success: function (html) {
-                    alert("U editua me sukses!");
-                    location.reload();
-                }
-            });
+    $(".edit").on("click", function () {
+        var ID = $(this).attr('id');
+        var td = $("td");
+        td.find("#results_" + ID).hide();
+        td.find("#editbox_" + ID).attr("style", "display:block");
+        td.find("#" + ID).attr("style", "display:inline");
+        $(this).addClass("hide");
     });
 
-$(".editbox").mouseup(function () {
-    return false;
-});
+    $(".save").click(function () {
+        var ID = $(this).attr("id");
+        var td = $("td");
+        var fileUrl = $("#url").attr("url");
+        var dataString = td.find("#editbox_" + ID).serialize();
+        $.ajax({
+            type: "POST",
+            url: fileUrl,
+            data: dataString,
+            cache: false,
+            dataType: "html",
+            async: false,
+            success: function () {
+                alert("U editua me sukses!");
+                location.reload();
+            }
+        });
+    });
 
-$(document).mouseup(function (){
-    $(".editbox, .save").hide();
-    $(".text").show();
-});
+    $(".editbox").mouseup(function () {
+        return false;
+    });
+
+    $(document).mouseup(function () {
+        $(".editbox, .save").hide();
+        $(".hide").removeClass("hide");
+        $(".text").show();
+    });
 });
