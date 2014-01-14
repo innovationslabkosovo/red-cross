61043ceeca53cda05439d3fb10468a558d544f0c
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2014-01-14 16:08:57 +0100

    update form_validate

--- js/form_validate.js
+++ js/form_validate.js
@@ -4,19 +4,17 @@ $(document).ready(function () {
     this_form.on("submit", function (e) {
         var postData = $(this).serializeArray();
         var formURL = $(this).attr('action');
-        $.ajax(
-            {
-                url: formURL,
-                type: 'POST',
-                data: postData,
-                success: function (data) {
-                    message.html('<pre><code class=\"prettyprint\">' + data + '</code></pre>');
-
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    message.html('<pre><code class=\"prettyprint\">AJAX Request Failed<br/> textStatus=' + textStatus + ', errorThrown=' + errorThrown + '</code></pre>');
-                }
-            });
+        $.ajax({
+            url: formURL,
+            type: 'POST',
+            data: postData,
+            success: function (data) {
+                message.html('<p>' + data + '</p>');
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                message.html('<p>AJAX Request Failed<br/> textStatus=' + textStatus + ', errorThrown=' + errorThrown + '<p>');
+            }
+        });
         e.preventDefault();
     });
 });
