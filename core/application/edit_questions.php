d14ab7a56839812801284e1d53c50cf20d4f87b9
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2014-01-16 13:05:59 +0100

    edit questions file

---
+++ core/application/edit_questions.php
@@ -0,0 +1,15 @@
+<?php
+include '../init.php';
+if ($_POST['id']) {
+	$id=mysql_real_escape_string($_POST['id']);
+	$question_desc=mysql_real_escape_string($_POST['question_description']);
+	$answers_description = $_POST['answers_description'];
+	$answers_id = $_POST['answers_id'];
+	mysql_query("update Question set description='$question_desc' where question_id='$id'");
+	ob_clean();
+	$i = 0;
+	foreach ($answers_id as $answer_id) {
+		mysql_query("update Answer set description='{$answers_description[$i++]}' where answer_id='{$answer_id}' AND question_id='$id'");
+	}
+	echo json_encode($_POST);
+}
