2c7447501bdcd4c8205412019c81392054c440d9
Visar <visar.mullafetah@kosovoinnovations.org>
2014-01-30 17:14:23 +0100

    removed test echo return classes and participants for a municipality

--- core/return_children_dropdown.php
+++ core/return_children_dropdown.php
@@ -8,8 +8,9 @@ if (isset($_POST['parent_value']) && isset($_POST['parent_id_field']) && isset($
     $child_id_field = $_POST['child_id_field'];
     $child_text_field = $_POST['child_text_field'];
 
+    if ($query_result=mysql_query("select $child_id_field , $child_text_field from $child_table where $parent_id_field = $parent_value"))
+        echo create_options($query_result , $child_id_field , $child_text_field);
+    else
+        echo "";
 }
-if ($query_result=mysql_query("select $child_id_field , $child_text_field from $child_table where $parent_id_field = $parent_value"))
-    echo create_options($query_result , $child_id_field , $child_text_field);
-else
-    echo "";
+
