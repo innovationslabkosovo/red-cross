ccf5c72ef122e34d2a5e70ae8ed30e60d871ac43
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2014-01-31 12:28:37 +0100

    declare public_reports variable

--- views/layout/aside.php
+++ views/layout/aside.php
@@ -1,10 +1,11 @@
 <aside id="Just_A_Random_ID">
     <?php
     $include_login = basename($_SERVER['PHP_SELF']);
-    if(logged_in() === false && $include_login === 'reports.php')
+    $public_reports = 'reports.php';
+    if(logged_in() === false && $include_login === $public_reports)
     {
 
-    } else if (logged_in() === false && $include_login != 'reports.php') {
+    } else if (logged_in() === false && $include_login != $public_reports) {
     	include $project_root.'views/user/login_view.php';
     }
     ?>
