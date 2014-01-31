5b14979725a83402207472ceb99c2d45f2f5de72
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2014-01-31 14:08:29 +0100

    dont include login view if report is public

--- views/layout/aside.php
+++ views/layout/aside.php
@@ -1,11 +1,10 @@
 <aside id="Just_A_Random_ID">
     <?php
     $include_login = basename($_SERVER['PHP_SELF']);
-    $public_reports = 'reports.php';
-    if(logged_in() === false && $include_login === $public_reports)
+    if(logged_in() === false && strpos(strtolower($include_login),'public'))
     {
 
-    } else if (logged_in() === false && $include_login != $public_reports) {
+    } else if (logged_in() === false && strpos(strtolower($include_login),'public') === false) {
     	include $project_root.'views/user/login_view.php';
     }
     ?>
