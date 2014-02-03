f8a68175dc9c71a6f33bf558d51cca7b886cd6d8
Lulzim Gashi <lulzim.gashi@kosovoinnovations.org>
2014-02-03 14:18:26 +0100

    testing

--- core/return_class_report.php
+++ core/return_class_report.php
@@ -20,8 +20,15 @@ if ($question_id) {
 if ($municipality_id) :
 	$sql = mysql_query("SELECT Class.location_id, Class.class_id, Class.name, Class.test_id, Location.municipality_id, Location.location_id $fields from Location INNER JOIN Class ON Class.location_id = Location.location_id $inner_join WHERE Location.municipality_id = $municipality_id".$where.$where_question);
 	if ($sql) :
-		while ($results = mysql_fetch_object($sql)) : ?>
+		while ($results = mysql_fetch_object($sql)) : pre($results); ?>
 				<option value="<?php echo $results->class_id; ?>"><?php echo $results->name; ?></option>
 		<?php endwhile; ?>
 	<?php endif; ?>
 <?php endif; ?>
+
+<!-- SELECT Class.location_id, Class.class_id, Class.name, Class.test_id, Location.municipality_id, Participant.name, Participant.surname, ParticipantAnswer.type, ParticipantAnswer.answer, Question.question_id, Question.description, Location.location_id from Location INNER JOIN Class ON Class.location_id = Location.location_id
+INNER JOIN ParticipantClass ON ParticipantClass.class_id = Class.class_id
+	INNER JOIN ParticipantAnswer ON ParticipantAnswer.participant_id = ParticipantClass.participant_id
+	INNER JOIN Participant ON Participant.participant_id = ParticipantAnswer.participant_id
+	INNER JOIN Question ON Question.question_id = ParticipantAnswer.question_id
+WHERE Location.municipality_id = 26 AND Class.class_id = 1 AND ParticipantAnswer.question_id = 1 -->
