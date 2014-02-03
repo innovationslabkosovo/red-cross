f093011b6b3fc02ffb0ab0397278fe4483d016f8
Ermal Berisha <errmal@gmail.com>
2014-02-03 09:48:09 +0100

    modified listing of categories and trainers

---
+++ views/create_trainer.php
@@ -0,0 +1,45 @@
+<?php
+$page_title = "Krijo Trajner te Ri";
+include '../core/init.php';
+protect_page();
+$errors = array();
+include $project_root . 'views/layout/header.php'; ?>
+
+    <form class="txfform-wrapper cf" name="trainer_form" action="../core/application/create_trainer.php" method="post">
+
+        <div class="row">
+            <h3>Shto trajner te ri</h3>
+            <div class="row">
+                <label>Emri i trajnerit te ri:</label><input type="text" placeholder="Emri" name="first_name" id="trainer">
+            </div>
+            <br>
+
+            <div class="row">
+                <label>Mbiemri i trajnerit te ri:</label><input type="text" placeholder="Mbiemri" name="last_name" id="trainer">
+            </div>
+            <br>
+
+            <div class="row">
+                <label>Emaili i trajnerit te ri:</label><input type="text" placeholder="Email" name="email" id="trainer">
+            </div>
+            <br>
+
+            <div class="row">
+                <label>Numri i telefonit:</label><input type="text" placeholder="Tel.No" name="phone" id="trainer">
+            </div>
+            <br>
+
+            <div class="row">
+                <input type="submit" value="Ruaj!">
+            </div>
+
+        </div>
+    </form>
+
+<?php
+if (isset($_GET['message']) && isset($_GET['object']))
+{
+    echo $display_messages[$_GET['object']][$_GET['message']];
+}
+include $project_root . 'views/layout/footer.php';
+?>
