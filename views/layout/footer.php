cf8588495d7c1cdcd85a8c3f1540f27ee317b107
Lulzim <lulzim.gashi92@gmail.com>
2014-02-14 16:14:42 +0100

    adding implemented by

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,7 +1,18 @@
 </div>
 <footer>
-    &copy; <a href="http://kosovoinnovations.org" target="_blank">UNICEF Innovations Lab Kosovo</a>. All rights reserved.
+	<div class="second">
+		Kryqi i Kuq i Kosovës
+	</div>
+    <div class="main_branch_footer" style="float: left;margin-left: 300px;margin-top: -20px;">
+    	Rr. Tirana nr 27 A<br>
+		+381 (0) 38 221 427, Fax  +381 (0) 38 242 284
+	</div>
+	<div class="footer-credits implemented" style="float:right;margin-top:-20px;">
+		<span>Implemented by</span> &nbsp; &nbsp;
+		<a href="http://www.kosovoinnovations.org/" target="_blank">
+			<img src="http://kosovoinnovations.org/themes/ilks/logo.png" alt="Innovations Lab" style="vertical-align:middle;height:30px;">
+		</a>
+	</div>
 </footer>
 </body>
 </html>
-
