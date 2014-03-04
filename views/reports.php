8952e5321a610b6e5fe3f60fcf835afc8b146269
Lulzim Gashi <lulzim.gashi92@gmail.com>
2014-03-04 14:17:21 +0100

    css changes

--- views/reports.php
+++ views/reports.php
@@ -79,7 +79,7 @@ if (is_admin($user_id)){
 <h2>Zgjedhni Periudhën për Raportin Komunal</h2>
 <form action="../core/application/periodic_municipal_report.php" method="POST">
     <div class="row">
-        <div class="dropdown">
+        <div class="dropdown" style="vertical-align: middle;">
             <select name="municipality" class="dropdown-select" data-validation="required">
                 <option value=0>Zgjedh Komunen
                     <?php
